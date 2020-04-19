5d41f069b0d7c60c286246397da293a3f7a0adac
Souvik Mandal <58888934+souvik362@users.noreply.github.com>
2020-04-19 17:30:20 +0530

    creating branches for different projects

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,8 @@ void main() => runApp(MaterialApp(
     ));
 
 
-class CardId extends StatefulWidget {
-  @override
-  _CardIdState createState() => _CardIdState();
-}
 
-class _CardIdState extends State<CardId> {
-  int ninjaLevel = 1;
+class CardId extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,87 +26,11 @@ class _CardIdState extends State<CardId> {
         centerTitle: true,
       ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 0),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('asset/logo2.jpg'),
-                  radius: 50.0,
-                ),
-              ),
-              Divider(
-                height: 60.0,
-                color: Colors.grey,
-              ),
-              Text(
-                'NAME',
-                style: TextStyle(
-                    color: Colors.black87,
-                    fontFamily: 'Ubuntu',
-                    letterSpacing: 2.0,
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              Text(
-                'Souvik Mandal',
-                style: TextStyle(
-                  color: Colors.amber,
-                  fontFamily: 'Ubuntu',
-                  letterSpacing: 2.0,
-                  fontSize: 25.0,
-                ),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Text(
-                'NINJA LEVEL',
-                style: TextStyle(
-                    color: Colors.black87,
-                    fontFamily: 'Ubuntu',
-                    letterSpacing: 2.0,
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              Text(
-                '$ninjaLevel',
-                style: TextStyle(
-                  color: Colors.amber,
-                  fontFamily: 'Ubuntu',
-                  letterSpacing: 2.0,
-                  fontSize: 25.0,
-                ),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Row(children: <Widget>[
-                Icon(Icons.email),
-                SizedBox(
-                  width: 8.0,
-                ),
-                Text('sam246@gmail.com',
-                    style: TextStyle(
-                        color: Colors.amber,
-                        fontSize: 20.0,
-                        fontFamily: 'Ubuntu'))
-              ])
-            ]),
+        padding: EdgeInsets.all(20.0),
+        child: Text('main'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            ninjaLevel +=1;
-          });
-        },
+        onPressed: (){},
         elevation: 10.30,
         backgroundColor:Colors.yellow[50],
        child: Icon(
