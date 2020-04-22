88dc77a7a13c69a82e021efa70e1c1d0d1c9863d
Souvik Mandal <58888934+souvik362@users.noreply.github.com>
2020-04-22 20:39:25 +0530

    Working with list and custom class in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
+import './quote.dart';
+void main() =>
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Myquote()));
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: NewList(),
-    ));
-class NewList extends StatelessWidget {
+class Myquote extends StatefulWidget {
+  @override
+  _MyquoteState createState() => _MyquoteState();
+}
+
+class _MyquoteState extends State<Myquote> {
+  List<Quotes> quoteList = [
+    Quotes(author:'catty', text: 'this the new quote'),
+    Quotes(author:'newton', text: 'the new thing  is a quote'),
+    Quotes(author:'oscar-wilde',text: 'make thing bigger'),
+  ];
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-      title: Text('new'),
-    ),
-    );
+      appBar: AppBar(
+        title: Text(
+          'Quote App',
+          style: TextStyle(fontFamily: 'ubuntu', fontSize: 25.0),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+          children: quoteList.map((quote) {
+            return Text(
+              '${quote.text} - ${quote.author}',  //here i use the quote class property
+            style: TextStyle(
+              fontSize:20.0
+            ),
+            );
+          }).toList()
+    )); 
   }
 }
---
+++ lib/quote.dart
@@ -0,0 +1,7 @@
+class Quotes {
+  String text;
+  String author;
+  Quotes({this.author, this.text}); //making this a named paramerter in constructor
+}
+
+
