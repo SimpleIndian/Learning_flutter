67eb8912677dcb1dd32e15497372c482cbe25a86
Souvik Mandal <58888934+souvik362@users.noreply.github.com>
2020-04-26 11:39:33 +0530

    Adding spinners from SpinKit Package

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_new_app/services/worldTime.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -7,38 +8,33 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
- 
-  
-
   void setTime() async {
-    WorldTime currentTime = WorldTime(location:'kolkata', url:'Asia/Kolkata',flag: 'get.png');
+    WorldTime currentTime =
+        WorldTime(location: 'kolkata', url: 'Asia/Kolkata', flag: 'get.png');
     await currentTime.getTime();
-   Navigator.pushReplacementNamed(context, '/home',arguments: {
-     'location':currentTime.location,
-     'flag':currentTime.flag,
-     'time':currentTime.time
-   }); //sending data to different route using arguments.
+     Navigator.pushReplacementNamed(context, '/home',arguments: {
+       'location':currentTime.location,
+       'flag':currentTime.flag,
+       'time':currentTime.time
+     }); //sending data to different route using arguments.
   }
 
   @override
   void initState() {
     super.initState();
     setTime();
-    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Text('Loading...☁☁☁',
-            style: TextStyle(
-              fontSize:25.0,
-              fontFamily:'ubuntu',
-              color:Colors.green
-            ),
-            ),
-          ),
-        ));
+      child: Center(
+        child: SpinKitChasingDots(
+          color: Colors.green,
+          size: 70.0,
+        ),
+      ),
+    ));
   }
 }
